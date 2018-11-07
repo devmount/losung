9608856d2ab7f5821517d5e750217aa49f6cd1a8
devmount <mail@devmount.de>
2018-11-07 10:42:23 +0100

    :repeat: init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,142 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container grid-xs">
+      <div class="columns">
+        <div class="column col-12 text-center mb-2">
+          <h1>Die Losungen</h1>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column col-12">
+
+          <div class="card">
+            <div class="card-header">
+              <div class="card-title h4">{{ data[1] }}, {{ data[0] }}</div>
+              <div class="card-subtitle text-gray">{{ data[2]}}</div>
+            </div>
+            <div class="card-body text-large">
+              <blockquote>
+                <p>{{ data[4] }}</p>
+                <cite class="float-right">— {{ data[3] }}</cite>
+              </blockquote>
+              <blockquote>
+                <p>{{ data[6] }}</p>
+                <cite class="float-right">— {{ data[5] }}</cite>
+              </blockquote>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="columns mt-3 pb-5">
+        <div class="column col-12 text-center text-gray">
+          <p>Die Losungen<br />&copy; Evangelische Brüder-Unität<br />Herrnhuter Brüdergemeine</p>
+        </div>
+      </div>
+    </div>
+
+    <footer>
+      <div class="container grid-xs">
+        <div class="columns">
+          <div class="column col-12 text-center">
+            <p>
+              <span class="d-block">Version 0.1.2</span>
+              <i class="icon icon-resize-horiz"></i> with love by <a href="https://twitter.com/devmount" target="_blank">Andreas Müller</a>.
+              <i class="icon icon-download ml-1"></i> on <a href="https://github.com/devmount/losung" target="_blank">GitHub</a>.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    var d = new Date()
+    return {
+      today: d,
+      data: ''
+    }
+  },
+  methods: {
+    getLosung (text) {
+      var lines = text.replace(/\//g, '').replace(/"/g, '').split(/\r?\n/)
+      for (let line of lines) {
+        var record = line.split(/\t/)
+        var date = record[0].split('.')
+        date = date[2] + '-' + date[1] + '-' + date[0]
+        if (date == this.today.toISOString().substring(0, 10)) {
+          this.data = record
+          break
+        }
+      }
+    }
+  },
+  mounted () {
+    var self = this
+    fetch('2018.csv')
+      .then(response => response.text())
+      .then(text => self.getLosung(text))
   }
 }
 </script>
 
 <style lang="scss">
+$primary-color: #30cef2;
+$error-color: #ed2f47;
+$body-font-color: #9ea4ad;
+$bg-color: #181a1b;
+$bg-color-light: #181a1b;
+$bg-color-dark: #2c3638;
+$border-color: #222627;
+$gray-color-dark: #595d65;
+
+@import "node_modules/spectre.css/src/spectre";
+@import "node_modules/spectre.css/src/spectre-icons";
+// @import "node_modules/spectre.css/src/spectre-exp";
+
+body {
+  background: #111;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 2em;
+  min-height: calc(100vh - 3em);
+  position: relative;
+
+  blockquote {
+    color: #c0c7d1;
+    border: none;
+    padding: 0 0 2em 0;
+    margin: 0 0 1em 0;
+
+    p {
+      margin: 0;
+    }
+  }
+
+  footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    
+    p {
+      color: $body-font-color;
+    }
+    a {
+      color: #eee;
+    }
+  }
+
+  .mt-3 {
+    margin-top: 1em;
+  }
+  .pb-5 {
+    padding-bottom: 5em;
+  }
 }
 </style>
